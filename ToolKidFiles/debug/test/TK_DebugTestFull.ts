7959197d206ea2227f4c12c046c90b9481ff474f
Der Pfandpirat <g.marquardt@teach-audio.com>
2024-06-05 16:46:40 +0200

    testlog

--- ToolKidFiles/debug/test/TK_DebugTestFull.ts
+++ ToolKidFiles/debug/test/TK_DebugTestFull.ts
@@ -143,6 +143,7 @@ interface TK_DebugTest_file {
         ToolKid.nodeJS.loopFiles(Object.assign({}, inputs, {
             execute: require
         }));
+        log(777,inputs);
         const summary = ToolKid.debug.test.getSummary(function(summary){
             logMissingSuspects(summary);
             summary.failures.forEach(logFailure);
