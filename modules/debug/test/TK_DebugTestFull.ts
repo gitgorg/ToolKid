b86838e468fa2cd60c1db8e76cad109fe3b3e76b
Der Pfandpirat <g.marquardt@teach-audio.com>
2026-03-25 17:08:25 +0100

    test log clarification

--- modules/debug/test/TK_DebugTestFull.ts
+++ modules/debug/test/TK_DebugTestFull.ts
@@ -159,6 +159,13 @@ interface TK_DebugTest_file {
     };
 
     const shortenData = function TK_DebugTestFull_shortenValue(list: any) {
+        if (typeof list === "function") {
+            const name = list.name;
+            list = list.toString();
+            if (name !== undefined && list.slice(0,10) === "function (") {
+                list = "function " + name + list.slice(9);
+            }
+        }
         return ToolKid.dataTypes.list.shorten({
             list,
             maxLength: (typeof list === "string" ? 200 : 30),
