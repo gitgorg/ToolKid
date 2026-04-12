353d9e4fd4acdc30ad54c92d9de057d65e8a5230
Der Pfandpirat <g.marquardt@teach-audio.com>
2026-04-12 20:29:44 +0200

    minor tag renaming

--- ToolKid.js
+++ ToolKid.js
@@ -808,7 +808,7 @@ fileCollection.set("LibraryParsing.js", module.exports);
             isROOTLayer: false,
         },
         //functions
-        cdw_funkDeclare: {
+        cdw_funkBody: {
             patterns: [["{:", ":}"]],
             contains: ["ROOT"]
         },
