70c6d5362b43976003e3883b690c92396bae6ac5
Der Pfandpirat <g.marquardt@teach-audio.com>
2026-09-23 12:18:03 +0200

    DTObject_clone

--- modules/dataTypes/TK_DataTypesObject.ts
+++ modules/dataTypes/TK_DataTypesObject.ts
@@ -2,6 +2,15 @@
 interface ToolKid_file { dataTypes: TK_DataTypes_file }
 interface TK_DataTypes_file {
     object: {
+        clone<Type>(
+            origin: Type[],
+            depth: number,
+        ): Type[],
+        clone<Type>(
+            origin: Type,
+            depth: number,
+        ): Type,
+
         filter<Data extends Dictionary, Key extends string>(
             inputs: {
                 data: Data;
@@ -22,6 +31,70 @@ interface TK_DataTypes_file {
 
 (function TK_DataTypesObject_init() {
     const publicExports = module.exports = <TK_DataTypes_file["object"]>{};
+
+    const clone = publicExports.clone =
+        <TK_DataTypes_file["object"]["clone"]>function TK_DataTypesObject_cloneDeep(
+            origin, depth
+        ) {
+            if (typeof origin !== "object" || origin === null) {
+                return origin; //origin is not clonable
+            };
+
+            if (origin instanceof Array) {
+                if (depth < 2) {
+                    return origin.slice(0);
+                }
+
+                depth -= 1;
+                const result = origin.slice(0);
+                const { length } = origin;
+                for (let i = 0; i < length; i += 1) {
+                    result[i] = clone(origin[i], depth);
+                }
+                return result;
+            }
+
+            if (origin instanceof Map) {
+                if (depth < 2) {
+                    return new Map(origin);
+                }
+
+                depth -= 1;
+                const result = new Map(origin);
+                result.forEach(function TK_DataTypesObject_cloneDeepMap(value, key) {
+                    result.set(key, clone(value, depth));
+                });
+                return result;
+            }
+
+            if (origin instanceof Set) {
+                if (depth < 2) {
+                    return new Set(origin);
+                }
+
+                depth -= 1;
+                const result = new Set();
+                result.forEach(function TK_DataTypesObject_cloneDeepMap(value) {
+                    result.add(clone(value, depth));
+                });
+                return result;
+            }
+
+            if (depth < 2) {
+                return Object.assign({}, origin);
+            }
+
+            depth -= 1;
+            const result = <Dictionary>Object.assign({}, origin);
+            const entries = Object.entries(origin);
+            const { length } = entries;
+            let keyValue: [string, any];
+            for (let i = 0; i < length; i += 1) {
+                keyValue = entries[i];
+                result[keyValue[0]] = keyValue[1];
+            }
+            return result;
+        };
 
     publicExports.filter = function TK_DataTypesObject_filter(inputs) {
         const result = {} as any;
